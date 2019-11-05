2a700489262ab7656ce19cc23cba942b8ab9dc0f
Jakob Fischer <jakobfischer93@gmail.com>
2019-11-05 17:36:33 +0100

    Some stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-scripts/build_image.sh && \
-scripts/build_elm.sh && \
-scripts/build_env.sh && \
+FILE="nginx/www/elm.js"
+elm make src/Main.elm --optimize --output=$FILE
+uglifyjs $FILE --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | \
+uglifyjs --mangle --output=$FILE
--- scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scripts/build_elm.sh && \
-scripts/build_env.sh && \
-scripts/run_container.sh
